77bc2d55f75973df311291f6c0ad533764feb561
oohyun15 <sakiss4774@gmail.com>
2021-11-07 19:49:23 +0900

    :sparkles: Add error handler methods

---
+++ helper/err.go
@@ -0,0 +1,24 @@
+package errhd
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func CheckErr(err error) {
+	if err := recover(); err != nil {
+		panic(err)
+	}
+}
+
+func CheckCode(res *http.Response, url string) {
+	defer func() {
+		if c := recover(); c != nil {
+			fmt.Println("recover", url)
+		}
+	}()
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
+}
